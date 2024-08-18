6630353269f56ec6d6ff15b1cfb67d898fdc4a96
Bùi Văn Luật <vanluatt2000@gmail.com>
2024-08-18 10:51:18 +0700

    Update 123

---
+++ MyWebPlay/Controllers/HomeController22.cs
@@ -0,0 +1,375 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebPlay.Extension;
+using MyWebPlay.Model;
+using System;
+using System.Collections;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace MyWebPlay.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        public ActionResult XuLySQL14()
+        {
+            try
+            {
+                TempData["urlCurrent"] = Request.Path.ToString().Replace("/Home/", "");
+                khoawebsiteClient(null);
+                var dua = System.IO.File.ReadAllText(Path.Combine(_webHostEnvironment.WebRootPath.Replace("\\wwwroot", ""), "PrivateFileAdmin", "Admin", "SecureSettingAdmin.txt")).Replace("\r", "").Split("\n")[14];
+                if (dua == "DIRECT_GOOGLE.COM_10_TIMES_AFTER_TO_COME_MYWEBPLAY_ON")
+                {
+                    if (this.HttpContext.Request.Method == "GET")
+                    {
+                        if (HttpContext.Session.GetObject<int>("google-trick-web") == null)
+                        {
+                            HttpContext.Session.SetObject("google-trick-web", 1);
+                            return Redirect("https://google.com");
+                        }
+                        else
+                        {
+                            var lan = HttpContext.Session.GetObject<int>("google-trick-web");
+                            if (lan != 10)
+                            {
+                                HttpContext.Session.SetObject("google-trick-web", lan + 1);
+                                return Redirect("https://google.com");
+                            }
+                        }
+                    }
+                }
+                if (TempData["locked-app"] == "true") return RedirectToAction("Error", "Home");
+                if (TempData["errorXY"] == "true") return Redirect("https://google.com");
+                if (TempData["TestTuyetDoi"] == "true") TempData["TestTuyetDoi"] = "true";
+                if (HttpContext.Session.GetString("TuyetDoi") != null)
+                {
+                    TempData["UyTin"] = "true";
+                    var td = HttpContext.Session.GetString("TuyetDoi");
+                    if (td == "true")
+                    {
+                        TempData["TestTuyetDoi"] = "true"; /*return View();*/
+                    }
+                    else
+                    {
+                        TempData["TestTuyetDoi"] = "false";
+                    }
+                }
+                if (TempData["tathoatdong"] == "true")
+                {
+                    return RedirectToAction("Error");
+                }
+                if (HttpContext.Session.GetString("userIP") == "0.0.0.0" && TempData["skipOK"] == "false") HttpContext.Session.Remove("userIP");
+                if (HttpContext.Session.GetString("userIP") == "0.0.0.0" && TempData["skipOK"] == "false") HttpContext.Session.Remove("userIP");
+                if (TempData["ClearWebsite"] == "true" /*|| TempData["UsingWebsite"] == "false" */ )
+                {
+                    HttpContext.Session.Remove("userIP");
+                    HttpContext.Session.SetString("userIP", "0.0.0.0");
+                    TempData["skipIP"] = "true";
+                }
+                TempData["urlCurrent"] = Request.Path.ToString().Replace("/Home/", "");
+                var listIP = new List<string>();
+
+                if (string.IsNullOrEmpty(HttpContext.Session.GetString("userIP")) == false)
+                    listIP.Add(HttpContext.Session.GetString("userIP"));
+                else
+                {
+                    TempData["GetDataIP"] = "true";
+                    return RedirectToAction("Index");
+                }
+                khoawebsiteClient(listIP);
+
+                ViewBag.NameCu = "user_id\r\nuser_name\r\nemail";
+                ViewBag.RecordCu = "USER001\tNguyen Thi Ga Trong\tgatrong@gmail.com";
+                ViewBag.NameMoi = "user_id\r\nuser_name\r\nphone_number\r\nemail\r\nuser_age";
+                ViewBag.RecordMoi = "USER002\tNguyen Van Ga Mai\t0987654321\tgamaide@gmail.com\t30";
+            }
+            catch (Exception ex)
+            {
+                var req = Request.Path;
+
+                if (req == "/" || string.IsNullOrEmpty(req))
+                    req = "/Home/Index";
+
+                HttpContext.Session.SetObject("error_exception_log", "[Exception/error log - " + req + " - " + Request.Method + " - " + ex.Source + "] : " + ex.Message + "\n\n" + ex.StackTrace);
+                return RedirectToAction("Error", new
+                {
+                    exception = "true"
+                });
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult XuLySQL14(IFormCollection f)
+        {
+            var nix = "";
+            var exter = false;
+            var linkdown = false;
+            try
+            {
+                var fix = "";
+                foreach (var item in f.Keys)
+                {
+                    fix += string.Format("{0} : {1}\n", item, f[item]);
+                }
+
+                HttpContext.Session.SetString("hanhdong_3275", fix);
+                var path = Path.Combine(_webHostEnvironment.WebRootPath.Replace("\\wwwroot", ""), "PrivateFileAdmin", "Admin", System.IO.File.ReadAllText(Path.Combine(_webHostEnvironment.WebRootPath.Replace("\\wwwroot", ""), "PrivateFileAdmin", "Admin", "SecureSettingAdmin.txt")).Replace("\r", "").Split('\n', StringSplitOptions.RemoveEmptyEntries)[4]);
+                var noidung = System.IO.File.ReadAllText(path);
+
+                var listSettingS = noidung.Replace("\r", "").Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+                var infoX = listSettingS[48].Split("<3275>", StringSplitOptions.RemoveEmptyEntries);
+
+                if (infoX[1] == "true")
+                    exter = true;
+                var infoY = listSettingS[8].Split("<3275>", StringSplitOptions.RemoveEmptyEntries);
+                if (infoY[1] == "true") linkdown = true;
+
+                var listIP = new List<string>();
+                ghilogrequest(f); if (exter == false)
+                {
+                    TempData["urlCurrent"] = Request.Path.ToString().Replace("/Home/", "");
+                    khoawebsiteClient(null);
+                    var dua = System.IO.File.ReadAllText(Path.Combine(_webHostEnvironment.WebRootPath.Replace("\\wwwroot", ""), "PrivateFileAdmin", "Admin", "SecureSettingAdmin.txt")).Replace("\r", "").Split("\n")[14];
+                    if (dua == "DIRECT_GOOGLE.COM_10_TIMES_AFTER_TO_COME_MYWEBPLAY_ON")
+                    {
+                        if (this.HttpContext.Request.Method == "GET")
+                        {
+                            if (HttpContext.Session.GetObject<int>("google-trick-web") == null)
+                            {
+                                HttpContext.Session.SetObject("google-trick-web", 1);
+                                return Redirect("https://google.com");
+                            }
+                            else
+                            {
+                                var lan = HttpContext.Session.GetObject<int>("google-trick-web");
+                                if (lan != 10)
+                                {
+                                    HttpContext.Session.SetObject("google-trick-web", lan + 1);
+                                    return Redirect("https://google.com");
+                                }
+                            }
+                        }
+                    }
+                    if (TempData["locked-app"] == "true") return RedirectToAction("Error", "Home");
+                    if (TempData["errorXY"] == "true") return Redirect("https://google.com");
+                    if (TempData["TestTuyetDoi"] == "true") TempData["TestTuyetDoi"] = "true";
+                    if (HttpContext.Session.GetString("TuyetDoi") != null)
+                    {
+                        TempData["UyTin"] = "true";
+                        var td = HttpContext.Session.GetString("TuyetDoi");
+                        if (td == "true")
+                        {
+                            TempData["TestTuyetDoi"] = "true"; /*return View();*/
+                        }
+                        else
+                        {
+                            TempData["TestTuyetDoi"] = "false";
+                        }
+                    }
+                    if (TempData["tathoatdong"] == "true")
+                    {
+                        return RedirectToAction("Error");
+                    }
+                    if (HttpContext.Session.GetString("userIP") == "0.0.0.0" && TempData["skipOK"] == "false") HttpContext.Session.Remove("userIP");
+                    if (HttpContext.Session.GetString("userIP") == "0.0.0.0" && TempData["skipOK"] == "false") HttpContext.Session.Remove("userIP");
+
+                    if (TempData["ClearWebsite"] == "true" /*|| TempData["UsingWebsite"] == "false" */ )
+                    {
+                        HttpContext.Session.Remove("userIP");
+                        HttpContext.Session.SetString("userIP", "0.0.0.0");
+                        TempData["skipIP"] = "true";
+                    }
+                    /*HttpContext.Session.Remove("ok-data");*/
+                    TempData["dataPost"] = "[POST]";
+                    HttpContext.Session.SetString("data-result", "true");
+                    TempData["urlCurrent"] = Request.Path.ToString().Replace("/Home/", "");
+
+                    if (string.IsNullOrEmpty(HttpContext.Session.GetString("userIP")) == false)
+                        listIP.Add(HttpContext.Session.GetString("userIP"));
+                    else
+                    {
+                        TempData["GetDataIP"] = "true";
+                        return RedirectToAction("Index");
+                    }
+                }
+
+                string namecu = f["txtNameCu"].ToString();
+                namecu = namecu.Replace("[T-PLAY]", "\t");
+                namecu = namecu.Replace("[N-PLAY]", "\n");
+                namecu = namecu.Replace("[R-PLAY]", "\r");
+
+                string recordcu = f["txtRecordCu"].ToString();
+                recordcu = recordcu.Replace("[T-PLAY]", "\t");
+                recordcu = recordcu.Replace("[N-PLAY]", "\n");
+                recordcu = recordcu.Replace("[R-PLAY]", "\r");
+
+                string namemoi = f["txtNameMoi"].ToString();
+                namemoi = namemoi.Replace("[T-PLAY]", "\t");
+                namemoi = namemoi.Replace("[N-PLAY]", "\n");
+                namemoi = namemoi.Replace("[R-PLAY]", "\r");
+
+                string recordmoi = f["txtRecordMoi"].ToString();
+                recordmoi = recordmoi.Replace("[T-PLAY]", "\t");
+                recordmoi = recordmoi.Replace("[N-PLAY]", "\n");
+                recordmoi = recordmoi.Replace("[R-PLAY]", "\r");
+
+
+                ViewBag.NameCu = f["txtNameCu"].ToString();
+                ViewBag.RecordCu = f["txtRecordCu"].ToString();
+                ViewBag.NameMoi = f["txtNameMoi"].ToString();
+                ViewBag.RecordMoi = f["txtRecordMoi"].ToString();
+
+                TempData["dataPost"] = "[" + recordmoi.Replace("\r", "\\r").Replace("\n", "\\n").Replace("\t", "\\t") + "]";
+
+                ghilogrequest(f); if (exter == false)
+                {
+                    khoawebsiteClient(listIP);
+                    HttpContext.Session.Remove("ok-data");
+                    Calendar xi = CultureInfo.InvariantCulture.Calendar;
+
+                    var xuxu = xi.AddHours(DateTime.UtcNow, 7);
+
+                    if (xuxu.Hour >= 6 && xuxu.Hour <= 17)
+                    {
+                        TempData["mau_background"] = "white";
+                        TempData["mau_text"] = "black";
+                        TempData["mau_nen"] = "dodgerblue";
+                        TempData["winx"] = "❤";
+                    }
+                    else
+                    {
+                        TempData["mau_background"] = "black";
+                        TempData["mau_text"] = "white";
+                        TempData["mau_nen"] = "rebeccapurple";
+                        TempData["winx"] = "❤";
+                    }
+                    var pathX = Path.Combine(_webHostEnvironment.WebRootPath.Replace("\\wwwroot", ""), "PrivateFileAdmin", "Admin", System.IO.File.ReadAllText(Path.Combine(_webHostEnvironment.WebRootPath.Replace("\\wwwroot", ""), "PrivateFileAdmin", "Admin", "SecureSettingAdmin.txt")).Replace("\r", "").Split('\n', StringSplitOptions.RemoveEmptyEntries)[4]);
+                    var noidungX = System.IO.File.ReadAllText(pathX);
+                    var listSetting = noidungX.Replace("\r", "").Split('\n', StringSplitOptions.RemoveEmptyEntries);
+                    var flag = 0;
+                    for (int i = 0; i < listSetting.Length; i++)
+                    {
+                        var info = listSetting[i].Split("<3275>", StringSplitOptions.RemoveEmptyEntries);
+
+                        if (flag == 0 && (info[0] == "Email_Upload_User" ||
+                            info[0] == "MegaIo_Upload_User" || info[0] == "Email_TracNghiem_Create" ||
+                            info[0] == "Email_TracNghiem_Update" || info[0] == "Email_Question" ||
+                            info[0] == "Email_User_Website" || info[0] == "Email_User_Continue" ||
+                            info[0] == "Email_Note" || info[0] == "Email_Karaoke"))
+                        {
+                            if (info[1] == "false")
+                            {
+
+                                TempData["mau_winx"] = "red";
+                                flag = 1;
+                            }
+                            else
+                            {
+
+                                TempData["mau_winx"] = "deeppink";
+                                flag = 0;
+                            }
+                        }
+                    }
+                }
+
+                var sox = "";
+                var cuctac = "";
+                var chim = "";
+
+                var s = "INSERT INTO XXX VALUES ('@replace')";
+
+                var htCu = new Hashtable();
+                var listCu = namecu.Replace("\r", "").Split("\n");
+                var reCu = recordcu.Split("\t");
+                for (int i = 0; i < listCu.Length; i++)
+                {
+                    htCu.Add(listCu[i], reCu[i]);
+                }
+
+                var htMoi = new Hashtable();
+                var listMoi = namemoi.Replace("\r", "").Split("\n");
+                var reMoi = recordmoi.Split("\t");
+                for (int i = 0; i < listMoi.Length; i++)
+                {
+                    htMoi.Add(listMoi[i], reMoi[i]);
+                }
+
+                var ketqua = new List<string>();
+                for(int i =0; i < listMoi.Length; i++)
+                {
+                    if (htCu.ContainsKey(listMoi[i]))
+                    {
+                        htMoi[listMoi[i]] = htCu[listMoi[i]];
+                    }
+                    ketqua.Add(htMoi[listMoi[i]].ToString());
+                }
+
+                var replace = string.Join("','", ketqua);
+
+                s = s.Replace("@replace", replace);
+
+                nix = s;
+
+                s = "<button id=\"click_copy\" onclick=\"copyResult()\"><b style=\"color:red\">COPY RESULT</b></button><br><br><textarea id=\"txtResultX\" style=\"color:blue\" rows=\"50\" cols=\"150\" readonly=\"true\" autofocus>" + s + "</textarea>";
+                Calendar x = CultureInfo.InvariantCulture.Calendar;
+
+                cuctac = x.AddHours(DateTime.UtcNow, 7).ToString();
+                chim = HttpContext.Request.Path.ToString().Replace("/", "").Replace("Home", "");
+                if (string.IsNullOrEmpty(chim)) chim = "Default";
+
+                sox = Path.Combine(_webHostEnvironment.WebRootPath, "POST_DataResult", cuctac.ToString().Replace("\\", "").Replace("/", "").Replace(":", "") + "_" + chim + "_dataresult.txt");
+                TempData["fileResult"] = cuctac.ToString().Replace("\\", "").Replace("/", "").Replace(":", "") + "_" + chim + "_dataresult.txt";
+                new FileInfo(sox).Create().Dispose();
+                System.IO.File.WriteAllText(sox, nix);
+                ViewBag.Result = s;
+
+                ViewBag.KetQua = "Thành công! Một kết quả đã được hiển thị ở cuối trang này!";
+                if (TempData["ConnectLinkDown"] == "true") return Redirect("/POST_DataResult/" + TempData["fileResult"]);
+
+                ghilogrequest(f); if (exter == false)
+                    return View();
+                else
+                {
+                    if (linkdown == true) return Redirect("/POST_DataResult/" + TempData["fileResult"]);
+                    return Ok(new
+                    {
+                        result = "http://" + Request.Host + "/POST_DataResult/" + TempData["fileResult"].ToString().Replace(" ", "%20")
+                    });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var req = Request.Path;
+
+                if (req == "/" || string.IsNullOrEmpty(req))
+                    req = "/Home/Index";
+
+                HttpContext.Session.SetObject("error_exception_log", "[Exception/error log - " + req + " - " + Request.Method + " - " + ex.Source + "] : " + ex.Message + "\n\n" + ex.StackTrace);
+                ghilogrequest(f); if (exter == false)
+                    return RedirectToAction("Error", new
+                    {
+                        exception = "true"
+                    });
+                return Ok(new
+                {
+                    error = HttpContext.Session.GetObject<string>("error_exception_log")
+                });
+            }
+            ghilogrequest(f); if (exter == false)
+                return View();
+            else
+            {
+                if (linkdown == true)
+                    return Redirect("/POST_DataResult/" + TempData["fileResult"]);
+                return Ok(new
+                {
+                    result = "http://" + Request.Host + "/POST_DataResult/" + TempData["fileResult"].ToString().Replace(" ", "%20")
+                });
+            }
+        }
+    }
+}

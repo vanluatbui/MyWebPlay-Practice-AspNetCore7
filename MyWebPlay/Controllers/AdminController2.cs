e135e38d771855094d3dc589181daab495e1e363
Bùi Văn Luật <vanluatt2000@gmail.com>
2025-01-31 16:57:47 +0700

    aaabbbccc

--- MyWebPlay/Controllers/AdminController2.cs
+++ MyWebPlay/Controllers/AdminController2.cs
@@ -2163,7 +2163,7 @@ namespace MyWebPlay.Controllers
             for (int i = 0; i < stringChars.Length; i++)
             {
                 var no = chars[random.Next(chars.Length)];
-                if (i == stringChars.Length - 1 && no == '\\')
+                if ((i == stringChars.Length - 1 && no == '\\') || (i == 0 && no == "<"))
                 {
                     i--;
                     continue;
